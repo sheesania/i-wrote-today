f0e455621424753a5cbdc243d9cb58539d2608dc
Alison Blomenberg <sheesania@users.noreply.github.com>
2021-02-07 20:19:49 -0700

    Color changes on click

--- src/Day.js
+++ src/Day.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+
 function Day(props) {
-  const isDayInCurrentMonth = typeof props.day !== 'undefined';
-  const enabled = isDayInCurrentMonth ? 'day-enabled' : 'day-disabled';
-  const number = isDayInCurrentMonth ? props.day.getDate() : '';
+  const [wroteToday, setWroteToday] = useState(false);
+
+  const dayInCurrentMonth = typeof props.day !== 'undefined';
+  const enabled = dayInCurrentMonth ? 'day-enabled' : 'day-disabled';
+  const number = dayInCurrentMonth ? props.day.getDate() : '';
+
+  let wroteTodayClass;
+  if (!dayInCurrentMonth) {
+    wroteTodayClass = '';
+  } else {
+    wroteTodayClass = wroteToday ? 'wrote-today' : 'didnt-write';
+  }
 
   return (
-    <td className={`day ${enabled}`}>
+    <td onClick={() => setWroteToday(!wroteToday)} className={`day ${enabled} ${wroteTodayClass}`}>
       <span className='number'>{number}</span>
     </td>
   );
