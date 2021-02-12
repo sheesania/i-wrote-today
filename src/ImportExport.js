21c1f5e78d89e534015ba3db5ef98a0069697bd1
Alison Blomenberg <sheesania@users.noreply.github.com>
2021-02-12 16:19:34 -0700

    text and styling for import/export

--- src/ImportExport.js
+++ src/ImportExport.js
@@ -1,7 +1,26 @@
+import './ImportExport.css';
+
 function ImportExport() {
   return (
     <div>
-      import/export
+      <h1>Import + export your data</h1>
+      <div className='page-content'>
+        <h2>Import</h2>
+        <p>
+          To import your data, open up an "iwrotetoday.txt" file that you exported earlier. Paste the contents here and click "Import". WARNING: All existing data will be overwritten!
+        </p>
+        <textarea></textarea>
+        <div className='button-parent'>
+          <button className='import-export-button'>Import</button>
+        </div>
+        <h2>Export</h2>
+        <p>
+          To export your data, click "Export" and download the "iwrotetoday.txt" file.
+        </p>
+        <div className='button-parent'>
+          <button className='import-export-button'>Export</button>
+        </div>
+      </div>
     </div>
   );
 }
